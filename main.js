9b260495f29d4f150dbcac5eb8afba5824939e58
Baronas0927 <do.baublys@gmail.com>
2024-06-19 11:02:12 +0300

    pokemon update

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 let ballsWeHave = 5;
 
+const apiUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const pokemon = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+
+fetch(apiUrl)
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network was not okay');
+        }
+        return  response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
 document.querySelector("#pokeballs").value = ballsWeHave; //hidden input value
 document.querySelector("#pokeballsDisplay").innerText = ballsWeHave; //display in HTML
 let pokeballsHolder = document.querySelector("#pokeballs");
+let rndPokeNo = 1 + Math.round(Math.random() * (248));
+console.log(rndPokeNo);
 
-document.querySelector("#btn").addEventListener("click", playGame);
-
-function playGame(){
+async function playGame(){
     let gameStatus = document.querySelector("#gameStart");
-    switch (gameStatus.value){
+    let pokemon = await fetchPokemon(rndPokeNo);
+    console.log(pokemon);
+    switch(gameStatus.value){
         case "1":
             document.querySelector("#btn-container").innerHTML = '<button class="btn btn-info" id="btn">pagauk pokemona!</button>';
             document.querySelector("#pokemon-container").innerHTML = '<div id="pokemon"></div>';
@@ -35,20 +55,37 @@ function playGame(){
             }
             break;
         case "3":
-            document.querySelector("#pokemon").innerHTML = '<h1>Pokemonas</h1>';
+            document.querySelector("#pokemon").innerHTML = '<h1>Pokemonas</h1> <img src="${pokemon}">';
             document.querySelector("#btn-container").innerHTML = "";
             gameStatus.value = 3;
             break;
         default:
             break;
     }
-    resetButton();
+     resetButton();
+    }
 
-}
+document.querySelector("#btn").addEventListener("click", playGame);
 
 function resetButton(){
-    if(document.querySelector("#btn") != null){
-        document.querySelector("#btn").removeEventListener("click", playGame);
-        document.querySelector("#btn").addEventListener("Click", playGame);
+    const btn = document.querySelector("#btn");
+    if(btn){
+        btn.removeEventListener("click", playGame);
+        btn.addEventListener("click", playGame);
     }
 }
+
+async function fetchPokemon(rndPokeNo){
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + rndPokeNo);
+    const data = await response.json();
+    return{
+        name: data.name,
+        photo: data.sprites.front_default,
+        stats: {
+            hp : Math.round(data.stats[0].base_stat * (0.7 + Math.random() * (1.3 - 0.7))),
+            attack : Math.round(data.stats[1].base_stat * (0.7 + Math.random() * (1.3 - 0.7))),
+            defence : Math.round(data.stats[2].base_stat * (0.7 + Math.random() * (1.3 - 0.7))),
+        }
+    };
+}
+document.querySelector("#btn").addEventListener("click", playGame);
